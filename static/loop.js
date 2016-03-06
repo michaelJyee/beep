0984e4b05d0f5101ec8560dd6ddb409a019a1f86
Michael Yee <MichaelJoshuaYee@gmail.com>
2016-03-06 18:03:19 -0500

    user input validation...

--- static/loop.js
+++ static/loop.js
@@ -9,6 +9,12 @@ var post = {
   bpm: 0
 }
 
+function returnInt(){
+    if (this.value.length > 4) {
+        this.value = this.value.slice(0,4); 
+    }
+}
+
 function toggle(){
   if($('input').val()){
     if($('#startStopButton').attr('value') === 'OFF' && $('input').val() >= 1){
@@ -137,4 +143,32 @@ $("#volumeIcon").click(function(){
     $('#my-input').simpleSlider("setValue", .75);
   }
 });
+
+function numbersonly(myfield, e, dec) {
+  var key;
+  var keychar;
+  if (window.event)
+    key = window.event.keyCode;
+  else if (e)
+    key = e.which;
+  else
+    return true;
+  keychar = String.fromCharCode(key);
+  // control keys 
+  if ((key==null) || (key==0) || (key==8) || (key==9) || (key==13) || (key==27) )
+    return true;
+  // numbers 
+  else if ((("0123456789").indexOf(keychar) > -1))
+    return true;
+  // decimal point jump 
+  else if (dec && (keychar == ".")) {
+    myfield.form.elements[dec].focus();
+    return false;
+  }
+  else return false;
+}
+
+
+
+
 
